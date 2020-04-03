f0edff93667cb623d40566ba682845d4fbe44c78
Fabio Silva <ffjs1993@gmail.com>
2020-04-03 22:24:13 +0100

    round-card component

---
+++ src/main/webapp/app/shared/round-card/round-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'round-card',
+  templateUrl: 'round-card.component.html',
+  styleUrls: ['./round-card.scss']
+})
+export class RoundCardComponent {
+  @Input() paddingTop = '10px';
+  @Input() paddingRight = '10px';
+  @Input() paddingBottom = '10px';
+  @Input() paddingLeft = '10px';
+  constructor() {}
+}
--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { IcamBackOfficeSharedLibsModule } from './shared-libs.module';
 import { AlertComponent } from './alert/alert.component';
 import { AlertErrorComponent } from './alert/alert-error.component';
 import { LoginModalComponent } from './login/login.component';
+import { RoundCardComponent } from './round-card/round-card.component';
 import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
 
 @NgModule({
   imports: [IcamBackOfficeSharedLibsModule],
-  declarations: [AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective],
+  declarations: [AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective, RoundCardComponent],
   entryComponents: [LoginModalComponent],
   exports: [IcamBackOfficeSharedLibsModule, AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective]
 })
