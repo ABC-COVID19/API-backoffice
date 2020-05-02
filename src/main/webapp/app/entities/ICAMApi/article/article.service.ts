b63b0d035d0e4c26a3ec679713b223bbae9159c1
Fabio Silva <ffjs1993@gmail.com>
2020-05-02 15:22:08 +0100

    article-service: addArticleToSpecialSourceRepo

--- src/main/webapp/app/entities/ICAMApi/article/article.service.ts
+++ src/main/webapp/app/entities/ICAMApi/article/article.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, concatMap } from 'rxjs/operators';
 import * as moment from 'moment';
 
 import { DATE_FORMAT } from 'app/shared/constants/input.constants';
 import { SERVER_API_URL } from 'app/app.constants';
 import { createRequestOption } from 'app/shared/util/request-util';
 import { IArticle } from 'app/shared/model/ICAMApi/article.model';
+import { SourceRepoService } from '../source-repo/source-repo.service';
 
 type EntityResponseType = HttpResponse<IArticle>;
 type EntityArrayResponseType = HttpResponse<IArticle[]>;
@@ -16,7 +17,7 @@ type EntityArrayResponseType = HttpResponse<IArticle[]>;
 export class ArticleService {
   public resourceUrl = SERVER_API_URL + 'services/icamapi/api/articles';
 
-  constructor(protected http: HttpClient) {}
+  constructor(protected http: HttpClient, protected repoService: SourceRepoService) {}
 
   create(article: IArticle): Observable<EntityResponseType> {
     const copy = this.convertDateFromClient(article);
@@ -57,6 +58,30 @@ export class ArticleService {
     return this.http
       .get<EntityResponseType>(`${this.resourceUrl}?revisionId.specified=false`)
       .pipe(map((res: EntityResponseType) => this.convertDateFromServer(res)));
+  }
+
+  addArticleToSpecialSourceRepo(article: IArticle): Observable<EntityResponseType> {
+    return this.repoService
+      .query({
+        'itemName.equals': 'special'
+      })
+      .pipe(
+        concatMap(r => {
+          const repos = r.body;
+
+          if (repos?.length) {
+            return of(repos[0].id);
+          }
+
+          return this.repoService
+            .create({
+              active: true,
+              itemName: 'special'
+            })
+            .pipe(map(newRepo => newRepo.body?.id));
+        }),
+        concatMap(id => this.create({ ...article, srepo: { id } }))
+      );
   }
 
   protected convertDateFromClient(article: IArticle): IArticle {
