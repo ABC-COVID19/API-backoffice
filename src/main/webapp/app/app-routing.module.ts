db05a2c9c3a50e9c04ff226a64c19aedcfa430ef
Fabio Silva <ffjs1993@gmail.com>
2020-05-03 17:55:38 +0100

    add login to /backoffice

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 import { errorRoute } from './layouts/error/error.route';
 import { Authority } from 'app/shared/constants/authority.constants';
 
-import { SidebarAndContentComponent } from 'app/layouts/sidebar-and-content/sidebar-and-content.component';
+// import { SidebarAndContentComponent } from 'app/layouts/sidebar-and-content/sidebar-and-content.component';
 import { TopNavbarAndContentComponent } from 'app/layouts/topnavbar-and-content/topnavbar-and-content.component';
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
 
@@ -12,41 +12,50 @@ const LAYOUT_ROUTES = [...errorRoute];
 @NgModule({
   imports: [
     RouterModule.forRoot([
+      // {
+      //   path: '',
+      //   children: [
+      //     {
+      //       path: '',
+      //       component: SidebarAndContentComponent,
+      //       children: [
+      //         {
+      //           path: '',
+      //           redirectTo: '/search',
+      //           pathMatch: 'full'
+      //         },
+      //         {
+      //           path: 'search',
+      //           loadChildren: () => import('./search/search.module').then(m => m.SearchModule)
+      //         },
+      //         {
+      //           path: 'about',
+      //           loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+      //         },
+      //         {
+      //           path: 'updates',
+      //           loadChildren: () => import('./icam-updates/icam-updates.module').then(m => m.IcamUpdatesModule)
+      //         },
+      //         {
+      //           path: 'articles',
+      //           loadChildren: () => import('./layouts/top-search/top-search.module').then(m => m.TopSearchModule)
+      //         }
+      //       ]
+      //     }
+      //   ]
+      // },
       {
         path: '',
-        children: [
-          {
-            path: '',
-            component: SidebarAndContentComponent,
-            children: [
-              {
-                path: '',
-                redirectTo: '/search',
-                pathMatch: 'full'
-              },
-              {
-                path: 'search',
-                loadChildren: () => import('./search/search.module').then(m => m.SearchModule)
-              },
-              {
-                path: 'about',
-                loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
-              },
-              {
-                path: 'updates',
-                loadChildren: () => import('./icam-updates/icam-updates.module').then(m => m.IcamUpdatesModule)
-              },
-              {
-                path: 'articles',
-                loadChildren: () => import('./layouts/top-search/top-search.module').then(m => m.TopSearchModule)
-              }
-            ]
-          }
-        ]
+        redirectTo: '/backoffice/articleList',
+        pathMatch: 'full'
       },
       {
         path: 'backoffice',
         component: TopNavbarAndContentComponent,
+        canActivate: [UserRouteAccessService],
+        data: {
+          authorities: [Authority.USER]
+        },
         children: [
           {
             path: '',
@@ -67,23 +76,23 @@ const LAYOUT_ROUTES = [...errorRoute];
           }
         ]
       },
-      {
-        path: 'legacy',
-        children: [
-          {
-            path: 'admin',
-            data: {
-              authorities: [Authority.ADMIN]
-            },
-            canActivate: [UserRouteAccessService],
-            loadChildren: () => import('./admin/admin-routing.module').then(m => m.AdminRoutingModule)
-          },
-          {
-            path: 'account',
-            loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
-          }
-        ]
-      },
+      // {
+      //   path: 'legacy',
+      //   children: [
+      //     {
+      //       path: 'admin',
+      //       data: {
+      //         authorities: [Authority.ADMIN]
+      //       },
+      //       canActivate: [UserRouteAccessService],
+      //       loadChildren: () => import('./admin/admin-routing.module').then(m => m.AdminRoutingModule)
+      //     },
+      //     {
+      //       path: 'account',
+      //       loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
+      //     }
+      //   ]
+      // },
       ...LAYOUT_ROUTES
     ])
   ],
